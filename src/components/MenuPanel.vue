e745a8d26550162bed55c09ae0e79150c096b93e
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2025-02-18 17:37:17 +0800

    google maps dark mode is enabled

--- src/components/MenuPanel.vue
+++ src/components/MenuPanel.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup></script>
 
 <template>
-  <main class="bg-zinc-950 text-white w-full h-full">
+  <main class="bg-zinc-950 text-white w-full h-full md:w-[412px]">
     <div class="p-4">
       <p class="font-bold text-2xl text-center">CFW</p>
     </div>
     <!-- header -->
     <div class="text-sm md:text-base border-zinc-600 border-t border-b">
       <div class="p-4 text-center">
-        <p>
+        <p class="text-xs">
           ULP prices from 6am <span class="bg-zinc-500 text-white px-2 rounded-lg">Today</span> and
           <span class="bg-zinc-700 text-white px-2 rounded-lg">Tomorrow</span>
         </p>
@@ -24,15 +24,18 @@
         </div>
       </div>
       <!-- prices today list -->
-      <section id="price-list-item" class="p-4 hover:bg-zinc-900 flex justify-between items-center">
+      <section
+        id="price-list-item"
+        class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+      >
         <div class="space-y-2">
           <!-- price -->
           <div>
             <p class="font-semibold">159.8</p>
-            <p class="text-zinc-400 text-xs">Today</p>
+            <p class="text-zinc-200 text-xs">Today</p>
           </div>
           <div>
-            <p class="font-semibold">159.8</p>
+            <p class="font-semibold text-zinc-400">212.9</p>
             <p class="text-zinc-400 text-xs">Tomorrow</p>
           </div>
           <!-- price -->
