c6781ad35ac9366a8b76f02144b80d174e5a427a
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2025-02-18 23:14:20 +0800

    need to work with api

--- src/components/MenuPanel.vue
+++ src/components/MenuPanel.vue
@@ -15,46 +15,396 @@
       </div>
     </div>
 
-    <!-- prices list -->
-    <section>
-      <div class="p-4 border-b border-zinc-600">
-        <div class="flex items-center space-x-2 text-md">
-          <i class="pi pi-list"></i>
-          <p>Today's price</p>
-        </div>
+    <div class="p-4 border-b border-zinc-600">
+      <div class="flex items-center space-x-2 text-md">
+        <i class="pi pi-list"></i>
+        <p>Today's price</p>
       </div>
-      <!-- prices today list -->
-      <section
-        id="price-list-item"
-        class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
-      >
-        <div class="space-y-2">
-          <!-- price -->
-          <div>
-            <p class="font-semibold">159.8</p>
-            <p class="text-zinc-200 text-xs">Today</p>
+    </div>
+    <!-- prices list -->
+
+    <main class="overflow-y-scroll h-full scrollbar-hide">
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
           </div>
           <div>
-            <p class="font-semibold text-zinc-400">212.9</p>
-            <p class="text-zinc-400 text-xs">Tomorrow</p>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
           </div>
-          <!-- price -->
-        </div>
-        <div>
-          <!-- details -->
-          <p class="font-bold">Shell Reddy Express Morely</p>
-          <p class="text-zinc-500">111 Broun Ave, PERTH</p>
-        </div>
-        <div>
-          <!-- logo -->
-          <img
-            src="../assets/shell.png"
-            class="rounded-lg w-[40px] h-[40px] object-contain"
-            width="40"
-            height="40"
-          />
-        </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
       </section>
-    </section>
+
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+      <section>
+        <!-- prices today list -->
+        <section
+          id="price-list-item"
+          class="border-b border-t border-zinc-800 p-4 hover:bg-zinc-900 flex justify-between items-center"
+        >
+          <div class="space-y-2">
+            <!-- price -->
+            <div>
+              <p class="font-semibold">159.8</p>
+              <p class="text-zinc-200 text-xs">Today</p>
+            </div>
+            <div>
+              <p class="font-semibold text-zinc-400">212.9</p>
+              <p class="text-zinc-400 text-xs">Tomorrow</p>
+            </div>
+            <!-- price -->
+          </div>
+          <div>
+            <!-- details -->
+            <p class="font-bold">Shell Reddy Express Morely</p>
+            <p class="text-zinc-500">111 Broun Ave, PERTH</p>
+          </div>
+          <div>
+            <!-- logo -->
+            <img
+              src="../assets/shell.png"
+              class="rounded-lg w-[40px] h-[40px] object-contain"
+              width="40"
+              height="40"
+            />
+          </div>
+        </section>
+      </section>
+    </main>
   </main>
 </template>
