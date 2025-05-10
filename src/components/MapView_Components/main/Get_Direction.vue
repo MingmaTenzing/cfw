a218961e5d664a28a9be5e17c0f4bae2ab0333e0
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mac.lan>
2025-05-10 16:16:04 +0800

    working on steps;

--- src/components/MapView_Components/main/Get_Direction.vue
+++ src/components/MapView_Components/main/Get_Direction.vue
@@ -1,5 +1,62 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+const site_address = route.params.address
+
+// navigator.geolocation.getCurrentPosition((position) => {
+//   console.log(position)
+// })
+onMounted(() => {})
+</script>
 
 <template>
-  <p>dfdf</p>
+  <main class="">
+    <!-- directions header -->
+    <div class="p-4 border-b border-border flex justify-between">
+      <p class="font-medium">Directions</p>
+      <i class="pi pi-times"></i>
+    </div>
+
+    <form class="space-y-4 p-4">
+      <!-- directions from -->
+      <div class="flex space-x-2 items-baseline">
+        <i class="pi pi-map-marker"></i>
+        <div class="">
+          <p class="text-primary/70">From</p>
+          <p class="font-semibold">Your Current Location</p>
+        </div>
+      </div>
+      <div class="flex space-x-2 items-baseline">
+        <i class="pi pi-map-marker"></i>
+        <div class="">
+          <p class="text-primary/70">To</p>
+          <p class="font-semibold">{{ site_address }}</p>
+        </div>
+      </div>
+    </form>
+
+    <div class="border border-border p-4">
+      <!-- distance and duration details -->
+      <div class="flex justify-between">
+        <div class="space-y-2">
+          <p class="text-primary/70">Distance</p>
+          <div class="flex items-center space-x-2">
+            <i class="pi pi-map"></i>
+            <p class="font-semibold">12.5 km</p>
+          </div>
+        </div>
+        <div class="space-y-2">
+          <p class="text-primary/70">Duration</p>
+          <div class="flex items-center space-x-2">
+            <i class="pi pi-clock"></i>
+            <p class="font-semibold">18mins</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
