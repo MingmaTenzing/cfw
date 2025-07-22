ac807e77ca96098931e82e3e42e0c485020b1b67
Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-07-22 12:39:37 +0800

    updated description and added screenshot for richer ui install

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,23 @@ export default defineConfig({
       manifest: {
         name: 'Check Fuel WA',
         short_name: 'CFW',
-        description: 'A PWA built with Vue 3',
+        description:
+          'Find the cheapest fuel prices across Western Australia. View it on Google Maps and get direction as well. Find all the trends happening in fuel prices',
         theme_color: '#ffffff',
+        screenshots: [
+          {
+            src: 'mobile-screenshot.png',
+            sizes: '540x720',
+            type: 'image/png',
+          },
+          {
+            src: 'desktop-screenshot.png',
+            sizes: '2460x1674',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+        ],
+
         icons: [
           {
             src: 'manifest-icon-192.maskable.png',
